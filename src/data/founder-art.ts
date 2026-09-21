51361aea4f0550be969e3ec073c52476d8cb91b7
Laurent Courtines <laurent1056@Mac-mini.attlocal.net>
2026-09-21 16:27:58 -0700

    feat(about): add founder portrait

---
+++ src/data/founder-art.ts
@@ -0,0 +1,6 @@
+export const founderPortrait = {
+  src: 'images/laurent-courtines-portrait.png',
+  width: 1122,
+  height: 1402,
+  alt: 'Portrait of Laurent Courtines, founder of Platonic Ideal',
+} as const
